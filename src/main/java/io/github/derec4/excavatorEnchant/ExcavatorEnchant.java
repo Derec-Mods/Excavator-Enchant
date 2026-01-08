638dc45cfc5932cee612645c061bba6a9aca30a9
Derek <derex1987@gmail.com>
2026-01-08 17:59:46 -0600

    feat: prepping datapack registration

--- src/main/java/io/github/derec4/excavatorEnchant/ExcavatorEnchant.java
+++ src/main/java/io/github/derec4/excavatorEnchant/ExcavatorEnchant.java
@@ -1,5 +1,8 @@
 package io.github.derec4.excavatorEnchant;
 
+import io.papermc.paper.datapack.Datapack;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import org.apache.maven.model.InputLocation;
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,9 +14,13 @@ public final class ExcavatorEnchant extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.DATAPACK_DISCOVERY, event -> {
+
+        });
+
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
-        // Use Adventure Components for console output (preferred on Paper)
+
         ConsoleCommandSender console = Bukkit.getConsoleSender();
         String version = getDescription().getVersion();
         console.sendMessage(Component.text("") );
