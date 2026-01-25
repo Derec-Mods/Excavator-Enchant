16e0722fc4759dfcadc0183d3b2fad563911250f
Derek <derex1987@gmail.com>
2026-01-25 05:52:08 -0600

    feat: registering datapack

--- src/main/java/io/github/derec4/excavatorEnchant/PluginBootstrap.java
+++ src/main/java/io/github/derec4/excavatorEnchant/PluginBootstrap.java
@@ -3,6 +3,10 @@ package io.github.derec4.excavatorEnchant;
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class PluginBootstrap implements io.papermc.paper.plugin.bootstrap.PluginBootstrap {
 
     @Override
@@ -10,6 +14,15 @@ public class PluginBootstrap implements io.papermc.paper.plugin.bootstrap.Plugin
         context.getLifecycleManager().registerEventHandler(LifecycleEvents.DATAPACK_DISCOVERY.newHandler(
                 event -> {
                     // All code is contained here.
+                    try {
+                        // Retrieve the URI of the datapack folder.
+                        URI uri = this.getClass().getResource("/excavator_enchant_helper").toURI();
+                        // Discover the pack. The ID is set to "provided", which indicates to
+                        // a server owner that your plugin includes this data pack.
+                        event.registrar().discoverPack(uri, "provided");
+                    } catch (URISyntaxException | IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
         ));
     }
